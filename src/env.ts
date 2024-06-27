f4466556ee272c125d466a0ba25d20fe90fa6cfb
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-06-27 17:29:03 -0400

    Semantics change

--- src/env.ts
+++ src/env.ts
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 import { v } from "./validator";
 dotenv.config();
 
-const envVariables = v.object({
+const envSchema = v.object({
   BOT_TOKEN: v.string().isNotEmpty(),
   APPLICATION_ID: v.string().isNotEmpty(),
   ORIGIN_URL: v.string().url(),
 });
 
-export const env = envVariables.parse(process.env);
+export const env = envSchema.parse(process.env);
