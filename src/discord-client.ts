de3d63528a151abdb40e620e54669f259beee0f8
JtheFox <60980739+JtheFox@users.noreply.github.com>
2024-06-30 15:12:27 -0400

    Add Discord gateway disconnect/reconnect handlers

--- src/discord-client.ts
+++ src/discord-client.ts
@@ -5,7 +5,6 @@ import {
   InteractionResponseType,
   MessageFlags,
   GatewayDispatchEvents,
-  GatewayVersion,
   PresenceUpdateStatus,
   ActivityType,
   GatewayOpcodes,
@@ -14,6 +13,8 @@ import {
   InteractionType,
   GatewayInteractionCreateDispatchData,
   GatewayHelloData,
+  GatewayVersion,
+  GatewayCloseCodes,
 } from "discord-api-types/v10";
 import { logger } from "./utils/logger";
 
@@ -22,62 +23,165 @@ const resolveBitfield = (bits: number[]) => {
   return bits.reduce((acc, bit) => acc | bit, 0);
 };
 
-const DISCORD_API_VERSION = "v10";
+const ApiVersion = "v10";
 
 export class DiscordClient {
-  token: string;
-  applicationId: string;
-  baseUrl = `https://discord.com/api/${DISCORD_API_VERSION}`;
-  forwardUrl: string;
+  /** Discord bot token */
+  readonly token = env.BOT_TOKEN;
+  /** Discord application id */
+  readonly applicationId = env.APPLICATION_ID;
+  /** https://discord.com/developers/docs/topics/gateway#connecting */
+  readonly gatewayUrl = `wss://gateway.discord.gg/?v=${GatewayVersion}&encoding=json`;
+  /** Base url for Discord API */
+  readonly baseUrl = `https://discord.com/api/${ApiVersion}`;
+  /** Base url for forwarded interactions */
+  readonly forwardUrl = env.ORIGIN_URL + "/api/discord/interactions";
+  /** Map of interaction messages to prevent rate limiting */
+  readonly interactionMessages: Map<string, number> = new Map();
+  /** WebSocket connection to Discord gateway */
   ws: WebSocket;
-  interactionMessages: Map<string, number> = new Map();
+  // For resuming connections
+  sessionId?: string;
+  resumeGatewayUrl?: string;
+  lastSequenceNumber?: number;
+  /** https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-close-event-codes */
+  shouldStartNewSession = [
+    GatewayCloseCodes.InvalidSeq,
+    GatewayCloseCodes.SessionTimedOut,
+  ];
+  shouldNotReconnect = [
+    GatewayCloseCodes.AuthenticationFailed,
+    GatewayCloseCodes.InvalidShard,
+    GatewayCloseCodes.ShardingRequired,
+    GatewayCloseCodes.InvalidAPIVersion,
+    GatewayCloseCodes.InvalidIntents,
+    GatewayCloseCodes.DisallowedIntents,
+  ];
 
   constructor(cb?: () => void) {
-    // Check for required environment variables
-    this.token = env.BOT_TOKEN;
-    this.applicationId = env.APPLICATION_ID;
-    this.forwardUrl = env.ORIGIN_URL + "/api/discord/interactions";
+    /** https://discord.com/developers/docs/topics/gateway#connecting */
+    const ws = new WebSocket(this.gatewayUrl);
+    this.ws = this.addSocketListeners(ws, cb);
+  }
 
-    // Initialize WebSocket
-    const ws = new WebSocket(
-      `wss://gateway.discord.gg/?v=${GatewayVersion}&encoding=json`
-    );
-
+  private addSocketListeners(ws: WebSocket, cb?: () => void) {
+    // Run initialize function after connection is open
     ws.on("open", () => {
-      this.initialize();
       cb?.();
     });
 
+    // Handle disconnects based on close code
+    ws.on("disconnect", (code: number) => {
+      logger.error("Disconnected from Discord gateway with code", code);
+
+      if (this.shouldNotReconnect.includes(code)) {
+        logger.error(
+          "The close code indicates that the socket should not attempt to reconnect"
+        );
+        return;
+      }
+
+      if (!this.ws.CLOSED) {
+        this.ws.close();
+      }
+
+      let url = this.gatewayUrl;
+      if (
+        this.shouldStartNewSession.includes(code) &&
+        this.sessionId &&
+        this.lastSequenceNumber
+      ) {
+        url = this.resumeGatewayUrl ?? url;
+        // Send resume payload
+        cb = () => {
+          ws.send(
+            JSON.stringify({
+              op: GatewayOpcodes.Resume,
+              d: {
+                token: this.token,
+                session_id: this.sessionId,
+                seq: this.lastSequenceNumber,
+              },
+            })
+          );
+        };
+      }
+
+      // Create new socket connection
+      const newSocket = this.addSocketListeners(new WebSocket(url), cb);
+      // Clear session data
+      this.sessionId = undefined;
+      this.resumeGatewayUrl = undefined;
+      this.lastSequenceNumber = undefined;
+      this.ws = newSocket;
+    });
+
+    /** https://discord.com/developers/docs/topics/gateway#gateway-events */
     ws.on("message", (data: any) => {
       const payload = JSON.parse(data);
-      const { t, op, d } = payload as GatewayReceivePayload;
+      const { t, op, d, s } = payload as GatewayReceivePayload;
 
-      if (op === GatewayOpcodes.Hello) {
-        this.heartbeat(d as GatewayHelloData);
+      // Handle gateway opcodes
+      switch (op) {
+        case GatewayOpcodes.Dispatch:
+          this.lastSequenceNumber = s;
+          break;
+        case GatewayOpcodes.Heartbeat:
+          this.ws.send(
+            JSON.stringify({
+              op: GatewayOpcodes.Heartbeat,
+              d: null,
+            })
+          );
+          break;
+        case GatewayOpcodes.Reconnect:
+          ws.close();
+          break;
+        case GatewayOpcodes.InvalidSession:
+          this.identify();
+          break;
+        /** https://discord.com/developers/docs/topics/gateway#hello-event */
+        case GatewayOpcodes.Hello:
+          this.identify();
+          this.heartbeat(d as GatewayHelloData);
+          break;
+        default:
+          break;
       }
 
-      if (t === GatewayDispatchEvents.Ready) {
-        const { user } = d;
-        logger.ready("Logged in as", user.username);
-      }
+      // Handle gateway dispatch events
+      switch (t) {
+        /** https://discord.com/developers/docs/topics/gateway#ready-event */
+        case GatewayDispatchEvents.Ready: {
+          const { user, session_id, resume_gateway_url } = d;
+          this.sessionId = session_id;
+          this.resumeGatewayUrl = resume_gateway_url;
+          logger.ready("Logged in as", user.username);
+          break;
+        }
+        case GatewayDispatchEvents.InteractionCreate: {
+          const interaction = d as GatewayInteractionCreateDispatchData;
 
-      if (t === GatewayDispatchEvents.InteractionCreate) {
-        const interaction = d as GatewayInteractionCreateDispatchData;
+          if (interaction?.application_id !== env.APPLICATION_ID) return;
+          logger.info("Received interaction", interaction.id);
 
-        if (interaction?.application_id !== env.APPLICATION_ID) return;
-        logger.info("Received interaction", interaction.id);
-
-        this.forwardInteraction(interaction);
+          this.forwardInteraction(interaction);
+          break;
+        }
+        default:
+          break;
       }
     });
 
-    this.ws = ws;
+    return ws;
   }
 
+  /** Returns new client instance */
   static start(cb?: () => void) {
     return new DiscordClient(cb);
   }
 
+  /** Reusable fetch init since all fetch requests in this class use the same init */
   private post(url: string, data: any) {
     return fetch(url, {
       method: "POST",
@@ -89,7 +193,10 @@ export class DiscordClient {
     });
   }
 
-  private async forwardInteraction(interaction: GatewayInteractionCreateDispatchData) {
+  /** Fowards Discord interaction data to main draft league app */
+  private async forwardInteraction(
+    interaction: GatewayInteractionCreateDispatchData
+  ) {
     // Function to reply with ephemeral error message
     const sendErrorMessage = (content: string) => {
       return this.post(
@@ -164,9 +271,10 @@ export class DiscordClient {
     }
   }
 
-  private initialize() {
+  /** https://discord.com/developers/docs/topics/gateway#identifying */
+  private identify(payload?: GatewayIdentify) {
     // Payload for initial handshake
-    const loginPayload: GatewayIdentify = {
+    const loginPayload: GatewayIdentify = payload ?? {
       op: GatewayOpcodes.Identify,
       d: {
         token: this.token,
@@ -198,6 +306,7 @@ export class DiscordClient {
     this.ws.send(JSON.stringify(loginPayload));
   }
 
+  /** https://discord.com/developers/docs/topics/gateway#sending-heartbeats */
   private heartbeat({ heartbeat_interval }: GatewayHelloData) {
     setInterval(() => {
       this.ws.send(
